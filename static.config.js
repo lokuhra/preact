1ee863d1d8c2b25a59857404cacabbeabdb38e62
lokuhra <leandro.88.bernardi@gmail.com>
2018-11-28 11:55:40 -0300

    9

--- static.config.js
+++ static.config.js
@@ -1,9 +1,6 @@
-import axios from 'axios'
-
 export default {
   preact: false,
   getRoutes: async () => {
-    const { data: posts } = await axios.get('https://jsonplaceholder.typicode.com/posts')
     return [
       {
         path: '/',
@@ -13,20 +10,6 @@ export default {
         path: '/about',
         component: 'src/containers/About',
       },
-      // {
-      //   path: '/blog',
-      //   component: 'src/containers/Blog',
-      //   getData: () => ({
-      //     posts,
-      //   }),
-      //   children: posts.map(post => ({
-      //     path: `/post/${post.id}`,
-      //     component: 'src/containers/Post',
-      //     getData: () => ({
-      //       post,
-      //     }),
-      //   })),
-      // },
       {
         is404: true,
         component: 'src/containers/404',
