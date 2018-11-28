dd092539d5f1e3929eb8c85f899ec979b142d1de
lokuhra <leandro.88.bernardi@gmail.com>
2018-11-27 21:43:13 -0300

    6

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import { withSiteData } from 'react-static'
 import A from '../components/A'
-//
-import logoImg from '../logo.png'
 
 export default withSiteData(() => (
   <div>
     <h1 style={{ textAlign: 'center' }}>Welcome to</h1>
-    <img src={logoImg} alt="" style={{ display: 'block', margin: '0 auto' }} />
       <A />
   </div>
 ))
